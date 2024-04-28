CherishOS/android_packages_apps_Cherish

Cherish: Re-enable UDFPS icon picker
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

src/com/cherish/settings/fragments/Udfps.java
@@ -38,6 +38,7 @@ import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.util.cherish.CherishUtils;
+import com.cherish.settings.fragments.UdfpsIconPicker;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -49,8 +50,10 @@ public class Udfps extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String UDFPS_CUSTOMIZATION = "udfps_customization";
+    private static final String KEY_UDFPS_ICONS = "udfps_icon_picker";
 
     private PreferenceCategory mUdfpsCustomization;
+    private Preference mUdfpsIcons;
     
     @Override
     public void onCreate(Bundle icicle) {
@@ -65,6 +68,13 @@ public class Udfps extends SettingsPreferenceFragment implements
         mUdfpsCustomization = (PreferenceCategory) findPreference(UDFPS_CUSTOMIZATION);
         if (!udfpsResPkgInstalled) {
             prefSet.removePreference(mUdfpsCustomization);
+        }
+
+        final boolean udfpsResIconPkgInstalled = CherishUtils.isPackageInstalled(getContext(),
+                "com.cherish.udfps.icons");
+        mUdfpsIcons = (Preference) findPreference(KEY_UDFPS_ICONS);
+        if (!udfpsResIconPkgInstalled) {
+            prefSet.removePreference(mUdfpsIcons);
         }
 
     }

src/com/cherish/settings/fragments/UdfpsIconPicker.java
@@ -1,0 +1,218 @@
+/*
+ * Copyright (C) 2022-2024 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cherish.settings.fragments;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.net.Uri;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceViewHolder;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.Indexable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class UdfpsIconPicker extends SettingsPreferenceFragment {
+
+    private RecyclerView mRecyclerView;
+
+    private Resources udfpsRes;
+
+    private String mPkg = "com.crdroid.udfps.icons";
+
+    private String[] mIcons;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.udfps_icon_picker_title);
+
+        loadResources();
+    }
+
+    private void loadResources() {
+        try {
+            PackageManager pm = getActivity().getPackageManager();
+            udfpsRes = pm.getResourcesForApplication(mPkg);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        mIcons = udfpsRes.getStringArray(udfpsRes.getIdentifier("udfps_icons",
+                "array", mPkg));
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(
+                R.layout.item_view, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        UdfpsIconAdapter mUdfpsIconAdapter = new UdfpsIconAdapter(getActivity());
+        mRecyclerView.setAdapter(mUdfpsIconAdapter);
+
+        return view;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.System.putIntForUser(resolver,
+                Settings.System.UDFPS_ICON, 0, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CHERISH_SETTINGS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public class UdfpsIconAdapter extends RecyclerView.Adapter<UdfpsIconAdapter.UdfpsIconViewHolder> {
+        Context context;
+        String mSelectedIcon;
+        String mAppliedIcon;
+
+        public UdfpsIconAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public UdfpsIconViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_option, parent, false);
+            UdfpsIconViewHolder vh = new UdfpsIconViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(UdfpsIconViewHolder holder, final int position) {
+            String iconRes = mIcons[position];
+
+            Glide.with(holder.image.getContext())
+                    .load("")
+                    .placeholder(getDrawable(holder.image.getContext(), mIcons[position]))
+                    .into(holder.image);
+
+            holder.image.setPadding(20,20,20,20);
+
+            holder.name.setVisibility(View.GONE);
+
+            if (position == Settings.System.getInt(context.getContentResolver(),
+                Settings.System.UDFPS_ICON, 0)) {
+                mAppliedIcon = iconRes;
+                if (mSelectedIcon == null) {
+                    mSelectedIcon = iconRes;
+                }
+            }
+            holder.itemView.setActivated(iconRes == mSelectedIcon);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    updateActivatedStatus(mSelectedIcon, false);
+                    updateActivatedStatus(iconRes, true);
+                    mSelectedIcon = iconRes;
+                    Settings.System.putInt(getActivity().getContentResolver(),
+                            Settings.System.UDFPS_ICON, position);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mIcons.length;
+        }
+
+        public class UdfpsIconViewHolder extends RecyclerView.ViewHolder {
+            TextView name;
+            ImageView image;
+            public UdfpsIconViewHolder(View itemView) {
+                super(itemView);
+                name = (TextView) itemView.findViewById(R.id.option_label);
+                image = (ImageView) itemView.findViewById(R.id.option_thumbnail);
+            }
+        }
+
+        private void updateActivatedStatus(String icon, boolean isActivated) {
+            int index = Arrays.asList(mIcons).indexOf(icon);
+            if (index < 0) {
+                return;
+            }
+            RecyclerView.ViewHolder holder = mRecyclerView.findViewHolderForAdapterPosition(index);
+            if (holder != null && holder.itemView != null) {
+                holder.itemView.setActivated(isActivated);
+            }
+        }
+    }
+
+    public Drawable getDrawable(Context context, String drawableName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            Resources res = pm.getResourcesForApplication(mPkg);
+            Context ctx = context.createPackageContext(
+                    mPkg, Context.CONTEXT_IGNORE_SECURITY);
+            return ctx.getDrawable(res.getIdentifier(drawableName, "drawable", mPkg));
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}

Mrick-stuffs/packages_apps_Evolver

src/com/evolution/settings/fragments/lockscreen/Udfps.java
@@ -50,6 +50,7 @@ import androidx.preference.TwoStatePreference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.util.evolution.EvolutionUtils;
+import com.evolution.settings.fragments.lockscreen.UdfpsIconPicker;
 
 import com.android.settings.R;
 import com.android.settings.Utils;
@@ -69,13 +70,16 @@ public class Udfps extends DashboardFragment implements
     private static final String TAG = "Udfps";
 
     private static final String UDFPS_CUSTOMIZATION = "udfps_customization";
+    private static final String KEY_UDFPS_ICONS = "udfps_icon_picker";
 
     private PreferenceCategory mUdfpsCustomization;
+    private Preference mUdfpsIcons;
 
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.evolution_settings_udfps;
     }
+
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -89,6 +93,13 @@ public class Udfps extends DashboardFragment implements
         mUdfpsCustomization = (PreferenceCategory) findPreference(UDFPS_CUSTOMIZATION);
         if (!udfpsResPkgInstalled) {
             prefScreen.removePreference(mUdfpsCustomization);
+        }
+
+         final boolean udfpsResIconPkgInstalled = EvolutionUtils.isPackageInstalled(getContext(),
+                "com.evolution.udfps.icons");
+        mUdfpsIcons = (Preference) findPreference(KEY_UDFPS_ICONS);
+        if (!udfpsResIconPkgInstalled) {
+            prefScreen.removePreference(mUdfpsIcons);
         }
     }
 

src/com/evolution/settings/fragments/lockscreen/UdfpsIconPicker.java
@@ -1,0 +1,218 @@
+/*
+ * Copyright (C) 2022-2024 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evolution.settings.fragments.lockscreen;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.net.Uri;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceViewHolder;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.Indexable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class UdfpsIconPicker extends SettingsPreferenceFragment {
+
+    private RecyclerView mRecyclerView;
+
+    private Resources udfpsRes;
+
+    private String mPkg = "com.evolution.udfps.icons";
+
+    private String[] mIcons;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.udfps_icon_picker_title);
+
+        loadResources();
+    }
+
+    private void loadResources() {
+        try {
+            PackageManager pm = getActivity().getPackageManager();
+            udfpsRes = pm.getResourcesForApplication(mPkg);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        mIcons = udfpsRes.getStringArray(udfpsRes.getIdentifier("udfps_icons",
+                "array", mPkg));
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(
+                R.layout.item_view, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        UdfpsIconAdapter mUdfpsIconAdapter = new UdfpsIconAdapter(getActivity());
+        mRecyclerView.setAdapter(mUdfpsIconAdapter);
+
+        return view;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.System.putIntForUser(resolver,
+                Settings.System.UDFPS_ICON, 0, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.EVOLVER;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public class UdfpsIconAdapter extends RecyclerView.Adapter<UdfpsIconAdapter.UdfpsIconViewHolder> {
+        Context context;
+        String mSelectedIcon;
+        String mAppliedIcon;
+
+        public UdfpsIconAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public UdfpsIconViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_option, parent, false);
+            UdfpsIconViewHolder vh = new UdfpsIconViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(UdfpsIconViewHolder holder, final int position) {
+            String iconRes = mIcons[position];
+
+            Glide.with(holder.image.getContext())
+                    .load("")
+                    .placeholder(getDrawable(holder.image.getContext(), mIcons[position]))
+                    .into(holder.image);
+
+            holder.image.setPadding(20,20,20,20);
+
+            holder.name.setVisibility(View.GONE);
+
+            if (position == Settings.System.getInt(context.getContentResolver(),
+                Settings.System.UDFPS_ICON, 0)) {
+                mAppliedIcon = iconRes;
+                if (mSelectedIcon == null) {
+                    mSelectedIcon = iconRes;
+                }
+            }
+            holder.itemView.setActivated(iconRes == mSelectedIcon);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    updateActivatedStatus(mSelectedIcon, false);
+                    updateActivatedStatus(iconRes, true);
+                    mSelectedIcon = iconRes;
+                    Settings.System.putInt(getActivity().getContentResolver(),
+                            Settings.System.UDFPS_ICON, position);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mIcons.length;
+        }
+
+        public class UdfpsIconViewHolder extends RecyclerView.ViewHolder {
+            TextView name;
+            ImageView image;
+            public UdfpsIconViewHolder(View itemView) {
+                super(itemView);
+                name = (TextView) itemView.findViewById(R.id.option_label);
+                image = (ImageView) itemView.findViewById(R.id.option_thumbnail);
+            }
+        }
+
+        private void updateActivatedStatus(String icon, boolean isActivated) {
+            int index = Arrays.asList(mIcons).indexOf(icon);
+            if (index < 0) {
+                return;
+            }
+            RecyclerView.ViewHolder holder = mRecyclerView.findViewHolderForAdapterPosition(index);
+            if (holder != null && holder.itemView != null) {
+                holder.itemView.setActivated(isActivated);
+            }
+        }
+    }
+
+    public Drawable getDrawable(Context context, String drawableName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            Resources res = pm.getResourcesForApplication(mPkg);
+            Context ctx = context.createPackageContext(
+                    mPkg, Context.CONTEXT_IGNORE_SECURITY);
+            return ctx.getDrawable(res.getIdentifier(drawableName, "drawable", mPkg));
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}